43bb69c36644aa71e590a9e8051d31b883674383
unknown-user <unknown-user@192.168.1.118>
2015-07-02 10:44:48 -0400

    Initial check-in of module Hello_Universal

---
+++ Hello_UniversalViewController.cs
@@ -0,0 +1,50 @@
+using UIKit;
+using CoreGraphics;
+using System;
+using Foundation;
+
+namespace Hello_Universal
+{
+	public partial class Hello_UniversalViewController : UIViewController
+	{
+		public Hello_UniversalViewController (string nibName, NSBundle bundle) : base (nibName, bundle)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			this.btnClickMe.TouchUpInside += (sender, e) => {
+				this.lblOutput.Text = "Hello, world! ";
+				this.lblOutput.Text = lblOutput.Text + "Clicked @ " + DateTime.Now.ToShortTimeString();
+			};
+		}
+
+		public override void ViewDidUnload ()
+		{
+			base.ViewDidUnload ();
+
+			// Release any retained subviews of the main view.
+			// e.g. this.myOutlet = null;
+		}
+
+		public override bool ShouldAutorotateToInterfaceOrientation (UIInterfaceOrientation toInterfaceOrientation)
+		{
+			// Return true for supported orientations
+			if (UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone) {
+				return (toInterfaceOrientation != UIInterfaceOrientation.PortraitUpsideDown);
+			} else {
+				return true;
+			}
+		}
+	}
+}
---
+++ Hello_UniversalViewController.designer.cs
@@ -0,0 +1,20 @@
+// WARNING
+//
+// This file has been generated automatically by MonoDevelop to store outlets and
+// actions made in the Xcode designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+
+namespace Hello_Universal
+{
+	[Register ("Hello_UniversalViewController")]
+	partial class Hello_UniversalViewController
+	{
+		[Outlet]
+		UIKit.UIButton btnClickMe { get; set; }
+
+		[Outlet]
+		UIKit.UILabel lblOutput { get; set; }
+	}
+}
